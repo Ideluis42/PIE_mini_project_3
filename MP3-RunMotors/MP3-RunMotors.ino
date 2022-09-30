8acf402e3201a4bdc3191662ecc8cf6f75b7ce74
Arturo Joya <98101322+ArturoJoya@users.noreply.github.com>
2022-09-30 11:07:00 -0400

    Create RunMotors
    A quick sketch made to run the motors forward
    TODO - Debug and get the motors working

---
+++ MP3-RunMotors/MP3-RunMotors.ino
@@ -0,0 +1,45 @@
+/*Firmware Implementation for the DC Motor Control Mini-Project for PIE
+Controller will send speed parameters? TBD
+Firmware will read the IR data to determine if robot is within the path limits ; adjust DC motor speeds (PWM) ; create micro corrections whenever IR signal detects the line.*/
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+#define leftIR 0;
+#define rightIR 1;
+
+#define defSpeed 150; 
+
+#define baud 9600
+#define serialDelay 20
+
+Adafruit_MotorShield MS = Adafruit_MotorShield();
+Adafruit_DCMotor *LM = MS.getMotor(1);
+Adafruit_DCMotor *RM = MS.getMotor(4);
+//disp(MS.getMotor(1))
+//disp(MS.getMotor(4))
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(baud)
+  MS.begin();
+  LM->setSpeed(defSpeed);
+  RM->setSpeed(defSpeed);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available >0){
+    nSpeed = Serial.read();
+    nSpeed = nSpeed.toInt();
+  }
+  leftIRread = analogRead(leftIR);
+  rightIRread = analogRead(rightIR);
+
+  LM->run(FORWARD);
+  RM->run(FORWARD);
+
+  delay(1000);
+
+}

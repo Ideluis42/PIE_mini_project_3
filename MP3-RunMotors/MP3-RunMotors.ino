07932ab9a81d6ffce2f1b4185cc0236def12d9e8
Arturo Joya <98101322+ArturoJoya@users.noreply.github.com>
2022-09-30 11:20:34 -0400

    Fixed Motor Port Numbers
    Updated the motor port numbers to match the ports on the actual implementation

--- MP3-RunMotors/MP3-RunMotors.ino
+++ MP3-RunMotors/MP3-RunMotors.ino
@@ -4,24 +4,24 @@ Firmware will read the IR data to determine if robot is within the path limits ;
 
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
+//#include "utility/Adafruit_MS_PWMServoDriver.h"
 
 #define leftIR 0;
 #define rightIR 1;
 
-#define defSpeed 150; 
+int defSpeed = 150; 
 
-#define baud 9600
-#define serialDelay 20
+#define baud 9600;
+#define serialDelay 20;
 
 Adafruit_MotorShield MS = Adafruit_MotorShield();
 Adafruit_DCMotor *LM = MS.getMotor(1);
-Adafruit_DCMotor *RM = MS.getMotor(4);
+Adafruit_DCMotor *RM = MS.getMotor(2);
 //disp(MS.getMotor(1))
 //disp(MS.getMotor(4))
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(baud)
+  //Serial.begin(baud);
   MS.begin();
   LM->setSpeed(defSpeed);
   RM->setSpeed(defSpeed);
@@ -30,12 +30,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(Serial.available >0){
-    nSpeed = Serial.read();
-    nSpeed = nSpeed.toInt();
-  }
-  leftIRread = analogRead(leftIR);
-  rightIRread = analogRead(rightIR);
+  //if(Serial.available >0){
+    //nSpeed = Serial.read();
+    //nSpeed = nSpeed.toInt();
+  //}
+  //leftIRread = analogRead(leftIR);
+  //rightIRread = analogRead(rightIR);
 
   LM->run(FORWARD);
   RM->run(FORWARD);
